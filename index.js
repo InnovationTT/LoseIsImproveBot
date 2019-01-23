c57c73fd5df8ca9e34eaebb7b248cd85e4e71165
InnovationTT <greylyngao@gmail.com>
2019-01-23 08:08:11 -0500

    4 bombs

--- index.js
+++ index.js
@@ -344,7 +344,7 @@ client.on("message", (message) => {
                 minBomberlvl++;
             }
             //message.channel.send("lvl: "+lvl+", hpgrowth: "+hpgrowth);
-            message.channel.send("Your base has a max hp of "+totalhp+". At sudden death, it will be set to "+constants.match_overtime_base_hp_ratio+"% of it's max hp, which is "+overtimehp+".\n"+" Your base will die in one full bombing (all 3 bombs hit) from a lvl "+minPPlanelvl+" Pelican Plane or a lvl "+minBomberlvl+" Bomber.");
+            message.channel.send("Your base has a max hp of "+totalhp+". At sudden death, it will be set to "+constants.match_overtime_base_hp_ratio+"% of it's max hp, which is "+overtimehp+".\n"+" Your base will die in one full bombing (all 4 bombs hit) from a lvl "+minPPlanelvl+" Pelican Plane or a lvl "+minBomberlvl+" Bomber.");
         } 
         // ask user to get help if they type nonsense hehexd
         else {
